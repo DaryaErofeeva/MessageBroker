9c8d1ce8266f497b86cc2f205ad7f883fac0a86a
Дарья Ерофеева <dyerofieieva@griddynamics.com>
2018-05-07 16:14:25 +0300

    Add watcher-module
    Change-Id: I8da3585cfa3e643a1394db479c2558fd411060b6

---
+++ watcher/src/main/java/com/griddynamics/internship/models/plural/Channels.java
@@ -0,0 +1,17 @@
+package com.griddynamics.internship.models.plural;
+
+import com.griddynamics.internship.models.Channel;
+
+import java.util.List;
+
+public class Channels {
+    private List<Channel> channels;
+
+    public List<Channel> getChannels() {
+        return channels;
+    }
+
+    public void setChannels(List<Channel> channels) {
+        this.channels = channels;
+    }
+}
