d2e9b199ed32ca62ca4bf3bd9e0c76797e9fd88f
Дарья Ерофеева <dyerofieieva@griddynamics.com>
2018-05-18 12:02:53 +0300

    Add logging to producer-module
    Change-Id: Ic2b193f30bb745680b88e320ad1bf65decc050b9

--- producer/src/main/java/com/griddynamics/internship/dead/letters/managing/QueueDeadLettersTask.java
+++ producer/src/main/java/com/griddynamics/internship/dead/letters/managing/QueueDeadLettersTask.java
@@ -1,5 +1,6 @@
 package com.griddynamics.internship.dead.letters.managing;
 
+import com.griddynamics.internship.LogService;
 import com.griddynamics.internship.dao.DAOFactory;
 import com.griddynamics.internship.resources.senders.QueueMessageSender;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,18 +9,22 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class QueueDeadLettersTask {
+
     @Autowired
     private DAOFactory daoFactory;
 
     @Autowired
     private QueueMessageSender queueMessageSender;
 
+    @Autowired
+    private LogService logService;
+
     @Scheduled(fixedRate = 50000)
     private void sendDeadMessage() {
         daoFactory.getQueueDAO().getAllFailed()
                 .forEach(queue -> queue.getMessages().forEach(
                         message -> {
-                            System.out.println("Trying to send dead queue-letter: " + message);
+                            logService.log("Trying to send dead queue-letter: {}", message);
                             queueMessageSender.sendMessage(queue, message);
                         }));
     }
