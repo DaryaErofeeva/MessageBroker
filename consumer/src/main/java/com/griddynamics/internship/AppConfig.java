6f4e490a7dcd4b2a6e9ad0aa55304866386282be
Дарья Ерофеева <dyerofieieva@griddynamics.com>
2018-05-22 17:02:36 +0300

    Add consumer-module tests
    Change-Id: I8bc5e835d97de340a6c1871e9bd833becd762e36

--- consumer/src/main/java/com/griddynamics/internship/AppConfig.java
+++ consumer/src/main/java/com/griddynamics/internship/AppConfig.java
@@ -2,19 +2,18 @@ package com.griddynamics.internship;
 
 import com.griddynamics.internship.models.factories.ConsumerFactory;
 import com.griddynamics.internship.models.response.ConsumerResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public ConsumerFactory consumerFactory() {
-        return new ConsumerFactory();
-    }
+    @Autowired
+    private ConsumerFactory consumerFactory;
 
     @Bean
     public ConsumerResponse consumerResponse() throws Exception {
-        return consumerFactory().getObject();
+        return consumerFactory.getObject();
     }
 }
--- consumer/src/main/java/com/griddynamics/internship/models/factories/ConsumerFactory.java
+++ consumer/src/main/java/com/griddynamics/internship/models/factories/ConsumerFactory.java
@@ -6,6 +6,7 @@ import com.griddynamics.internship.models.response.ConsumerResponse;
 import com.griddynamics.internship.models.response.ResponseMessage;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
@@ -21,10 +22,13 @@ public class ConsumerFactory implements FactoryBean<ConsumerResponse> {
     @Autowired
     private LogService logService;
 
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
     @Override
     public ConsumerResponse getObject() throws Exception {
         try {
-            return new RestTemplate()
+            return restTemplateBuilder.build()
                     .postForObject("http://localhost:8080/broker/v1/consumer",
                             new HttpEntity<>(consumerRequest), ConsumerResponse.class);
         } catch (HttpClientErrorException ex) {
